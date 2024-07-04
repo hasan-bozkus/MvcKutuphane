cd7974c6021b90f8aa448cad384337ef348a667c
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-07-04 13:31:17 +0300

    Mvc 5 ile 3 Panelli Kütüphane Yönetim Sistemi
    Bölüm 1 - Veri Tabanı ve Tablolar:
    Veri Tabanı ve Tablo Oluşturma,
    Veri Tabanı Oluşturma,
    Diyagram ve İlişkiler,
    Proje Dosya Linki
    
    Bölüm 2 - Model View Controller ve Kategoriler:
    Projenin Oluşturulması,
    Modelin Kurulması,
    Class ve Propertyler,
    View ve Layout,
    Sidebar Kurulması,
    SideBarın Hazırlanması.
    Kategori Controller Oluşturma,
    Table Kullanımı,
    Kategorilerin Listelenmesi,
    Kategori Ekleme,
    HTTPGET HTTPPOST,
    Kategori Silme,
    Kategori Bilgilerini Taşıma,
    Kategori Güncelleme

---
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entities;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KategoriController : Controller
+    {
+        MvcKutuphaneYonetimSistemiEntities db = new MvcKutuphaneYonetimSistemiEntities();
+
+
+        public ActionResult Index()
+        {
+            var values = db.Kategoriler.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(Kategoriler kategoriler)
+        {
+            db.Kategoriler.Add(kategoriler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var values = db.Kategoriler.Find(id);
+            db.Kategoriler.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult KategoriGetir(int id)
+        {
+            var values = db.Kategoriler.Find(id);
+            return View("KategoriGetir", values);
+        }
+
+        [HttpPost]
+        public ActionResult KategoriGetir(Kategoriler kategoriler)
+        {
+            var values = db.Kategoriler.Find(kategoriler.KategoriID);
+            values.KategoriAd = kategoriler.KategoriAd;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
