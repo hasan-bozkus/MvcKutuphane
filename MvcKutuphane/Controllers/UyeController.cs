2aec75cd740b6d371fac52f34596b37f7213e081
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-07-22 14:47:57 +0300

    Mvc 5 ile 3 Panelli Kütüphane Yönetim Sistemi
    Bölüm 6 - Üye İşlemleri:
    Üye Listeleme,
    Üye Ekleme İşlemi,
    Üyeleri Sayfalama ile Listeleme,
    Üye Silme ve Güncelleme

---
+++ MvcKutuphane/Controllers/UyeController.cs
@@ -0,0 +1,73 @@
+﻿using MvcKutuphane.Models.Entities;
+using PagedList;
+using PagedList.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class UyeController : Controller
+    {
+
+        MvcKutuphaneYonetimSistemiEntities db = new MvcKutuphaneYonetimSistemiEntities();
+    
+        public ActionResult Index(int page = 0)
+        {
+            //var values = db.Uyeler.ToList();
+            var values = db.Uyeler.ToList().ToPagedList(page, 8);
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult UyeEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult UyeEkle(Uyeler uyeler)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            db.Uyeler.Add(uyeler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UyeSil(int id)
+        {
+            var result = db.Uyeler.Find(id);
+            db.Uyeler.Remove(result);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UyeGuncelle(int id)
+        {
+            var result = db.Uyeler.Find(id);
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult UyeGuncelle(Uyeler uyeler)
+        {
+            var result = db.Uyeler.Find(uyeler.UyeID);
+            result.UyeAd = uyeler.UyeAd;
+            result.UyeSoyad = uyeler.UyeSoyad;
+            result.KullaniciAd = uyeler.KullaniciAd;
+            result.Fotograf = uyeler.Fotograf;
+            result.EMail = uyeler.EMail;
+            result.UyeOkul = uyeler.UyeOkul;
+            result.UyeAd = uyeler.UyeAd;
+            result.Telefon = uyeler.Telefon;
+            return View();
+        }
+    }
+}
