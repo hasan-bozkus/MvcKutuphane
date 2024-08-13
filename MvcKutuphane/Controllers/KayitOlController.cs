d63a5eb3a5a10f4414eb9b2e4ca50869a0a9f98f
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-08-13 15:27:40 +0300

    Mvc 5 ile 3 Panelli Kütüphane Yönetim Sistemi
    Bölüm 15 - Kullanıcı Paneli İçin Giriş ve Kayit Sayfaları:
    LoginController,
    Register Formu Düzenlemesi,
    Yeniden Merhaba,
    Login Paneli Düzenleme

---
+++ MvcKutuphane/Controllers/KayitOlController.cs
@@ -0,0 +1,33 @@
+﻿using MvcKutuphane.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcKutuphane.Controllers
+{
+    public class KayitOlController : Controller
+    {
+        MvcKutuphaneYonetimSistemiEntities db = new MvcKutuphaneYonetimSistemiEntities();
+
+        [HttpGet]
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Kayit(Uyeler uyeler)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            db.Uyeler.Add(uyeler);
+            db.SaveChanges();
+            return RedirectToAction("GirisYap", "Login");
+        }
+    }
+}
