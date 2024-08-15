c12d93a75ad541403a742a3372eca465f0fd820c
Hasan BOZKUŞ <103758391+hasan-bozkus@users.noreply.github.com>
2024-08-15 11:54:02 +0300

    Mvc 5 ile 3 Panelli Kütüphane Yönetim Sistemi
    Bölüm 17 - Login İşlemi:
    Authorize İşlemi,
    Login İşlemi Giriş Yapma,
    Mail Adresini Profile Taşıma,
    Profil Düzenlemeleri,
    Temapdata ile Verileri Profile Taşıma,
    Şifre Güncelleme,
    Session Değişikliği,
    Şifre Değiştirme Yeni Yöntem,
    Profil Bilgilerini Güncelleme

--- MvcKutuphane/Controllers/LoginController.cs
+++ MvcKutuphane/Controllers/LoginController.cs
@@ -1,17 +1,43 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace MvcKutuphane.Controllers
 {
     public class LoginController : Controller
     {
 
+        MvcKutuphaneYonetimSistemiEntities db = new MvcKutuphaneYonetimSistemiEntities();
+
+        [HttpGet]
         public ActionResult GirisYap()
         {
             return View();
+        }
+
+        public ActionResult GirisYap(Uyeler uyeler)
+        {
+            var values = db.Uyeler.FirstOrDefault(x => x.EMail == uyeler.EMail && x.Sifre == uyeler.Sifre);
+            if (values != null)
+            {
+                FormsAuthentication.SetAuthCookie(values.EMail, false);
+                Session["Email"] = values.EMail.ToString();
+                //TempData["UyeID"] = values.UyeID.ToString();
+                //TempData["UyeAd"] = values.UyeAd.ToString();
+                //TempData["UyeSoyad"] = values.UyeSoyad.ToString();
+                //TempData["KullaniciAd"] = values.KullaniciAd.ToString();
+                //TempData["Sifre"] = values.Sifre.ToString();
+                //TempData["UyeOkul"] = values.UyeOkul.ToString();
+                return RedirectToAction("Index", "Panelim");
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- MvcKutuphane/Controllers/PanelimController.cs
+++ MvcKutuphane/Controllers/PanelimController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcKutuphane.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,31 @@ namespace MvcKutuphane.Controllers
     public class PanelimController : Controller
     {
 
+        MvcKutuphaneYonetimSistemiEntities db = new MvcKutuphaneYonetimSistemiEntities();
 
+        [Authorize]
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            var memberMail = (string)Session["Email"];
+            var values = db.Uyeler.FirstOrDefault(z => z.EMail == memberMail);
+
+            return View(values);
+        }
+
+        [HttpPost]
+        public ActionResult Index2(Uyeler uyeler)
+        {
+            var user = (string)Session["EMail"];
+            var member = db.Uyeler.FirstOrDefault(x=> x.EMail == user);
+            member.Sifre = uyeler.Sifre;
+            member.UyeAd = uyeler.UyeAd;
+            member.UyeSoyad = uyeler.UyeSoyad;
+            member.UyeOkul = uyeler.UyeOkul;
+            member.KullaniciAd = uyeler.KullaniciAd;
+            member.Fotograf = uyeler.Fotograf;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
